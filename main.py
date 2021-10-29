a1e9a95f8b00773148766ef8b590a8204596d9fd
Esther Hur <yhur6@binghamton.edu>
2021-10-29 14:54:39 -0400

    Updated main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 
 def main():
     pygame.init()
-    team = {"lead": "Christopher Yu", "backend": "Esther Hur", "frontend": "Ashley Yu"}
+    team = {"lead": "Christopher Yu", "backend": "Young Seo (Esther) Hur", "frontend": "Ashley Yu"}
     print("Software Lead is:", team["lead"])
     print("Backend is:", team["backend"])
     print("Frontend is:" , team["frontend"])
